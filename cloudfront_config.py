29132df10981d3739b2351a61a60c4bd0e45b0c6
davidshtian <david881223@outlook.com>
2022-07-06 12:24:01 +0800

    Add configuration
    For boto3 session, s3 bucket and acm cert

--- cloudfront_config.py
+++ cloudfront_config.py
@@ -12,7 +12,6 @@ import botocore.exceptions
 from datetime import datetime, timezone
 import argparse
 import logging
-import json
 
 """
 Instructions
@@ -23,26 +22,21 @@ Before running this script, making sure you have access to:
 
 Running the script as an example
 
-$ python3 cloudfront_config.py --domain service.yuhong.com --origin ec2-54-90-38-64.compute-1.amazonaws.com --dist_ref E2Z4DFMIXLKSQP
+$ python3 cloudfront_config.py --domain service.yuhong.com --origin ec2-54-90-38-64.compute-1.amazonaws.com --dist_ref E2Z4DFMIXLKSQP --log_bucket your-bucket --log_prefix abc --profile default
 """
 # Set up our logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-def loadConfig():
-    fp = open("./config.json")   
-    data = json.load(fp)
-    return(data)
+def get_reference_config(ref_dist, boto3_session):
+    client = boto3_session.client('cloudfront')
+    try:
+        return client.get_distribution_config(Id=ref_dist)
+    except botocore.exceptions.ClientError as error:
+        logger.exception(f"{format(error)}")
+        raise error
 
-def get_reference_config(ref_dist):
-	client = boto3.client('cloudfront')
-	try:
-		return client.get_distribution_config(Id=ref_dist)
-	except botocore.exceptions.ClientError as error:
-		logger.exception(f"{format(error)}")
-		raise error
-
-def get_certificate_mapping():
+def get_certificate_mapping(boto3_session):
 	my_config = Config(
 	   region_name = 'us-east-1'
 	   #  signature_version = 'v4',
@@ -51,7 +45,7 @@ def get_certificate_mapping():
 	   #      'mode': 'standard'
     # }
     )
-	client = boto3.client('acm', config=my_config)
+	client = boto3_session.client('acm', config=my_config)
 	# paginator = client.get_paginator('list_certificates')
 	try:
 		# cert_dict = {}
@@ -117,42 +111,48 @@ def set_config_based_on_ref(ref_config, conf_domain, conf_origin, certArn, bucke
 	config['Origins']['Items'][0]['Id'] = conf_origin
 	config['Origins']['Items'][0]['DomainName'] = conf_origin
 	config['DefaultCacheBehavior']['TargetOriginId'] = conf_origin
-	config['Comment'] = conf_domain
-	config['ViewerCertificate']['ACMCertificateArn'] = certArn
+	config['Comment'] = conf_domain 
+	if config['ViewerCertificate']['CloudFrontDefaultCertificate'] is False:
+		config['ViewerCertificate']['ACMCertificateArn'] = certArn
 	if 'Logging' in config:
 		if config['Logging']['Enabled'] == True:
-			config['Logging']['Bucket'] = bucket
+			config['Logging']['Bucket'] = bucket + ".s3.amazonaws.com"
 			config['Logging']['Prefix'] = prefix
 	return(config) 
 
-def create_distribution(config):
-	client = boto3.client('cloudfront')
-	try:
-		distribution = client.create_distribution(DistributionConfig=config)
-		logger.info(f"Done! Created distribution {format(distribution['Distribution']['Id'])}.")
-	except botocore.exceptions.ClientError as error:
-		logger.exception(f"{format(error)}")
-		raise error
+def create_distribution(config, boto3_session):
+    client = boto3_session.client('cloudfront')
+    try:
+        distribution = client.create_distribution(DistributionConfig=config)
+        logger.info(f"Done! Created distribution {format(distribution['Distribution']['Id'])}.")
+    except botocore.exceptions.ClientError as error:
+        logger.exception(f"{format(error)}")
+        raise error
 
 
 if __name__ == "__main__":
-	data = loadConfig() # s3 logging
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--domain', type=str, required=True)
 	parser.add_argument('--origin', type=str, required=True)
 	parser.add_argument('--dist_ref', type=str, required=True)
+	parser.add_argument('--log_bucket', type=str, required=True)
+	parser.add_argument('--log_prefix', default='', type=str, required=False)
+	parser.add_argument('--profile', type=str, required=True)
 	args = parser.parse_args()
 
 	conf_domain = args.domain 
 	conf_origin = args.origin
 	conf_ref_dist = args.dist_ref
+	conf_profile = args.profile
+	conf_log_bucket = args.log_bucket
+	conf_log_prefix = args.log_prefix
+	boto3_session = boto3.Session(profile_name=conf_profile)
 
-	ref_config = get_reference_config(conf_ref_dist)
+	ref_config = get_reference_config(conf_ref_dist, boto3_session)
 
-	certs = get_certificate_mapping()
+	certs = get_certificate_mapping(boto3_session)
 	certArn = get_certificate_arn(certs, conf_domain)
 
-	config = set_config_based_on_ref(ref_config, conf_domain, conf_origin, certArn, data['logBucket'], data['logPrefix'])
+	config = set_config_based_on_ref(ref_config, conf_domain, conf_origin, certArn, conf_log_bucket, conf_log_prefix)
 	# print(config)
-	create_distribution(config)
-
+	create_distribution(config, boto3_session)
